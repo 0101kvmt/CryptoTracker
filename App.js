3741efd7915060970940940e810c95926b8b3827
0101kvmt <0101kvmt@gmail.com>
2018-01-03 16:20:56 -0800

    first commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Header } from './src/components'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Header />
+        <Text>REAL-TIME Crypto</Text>
+        <Text>REAL-TIME HODLS</Text>
+      </View>
+
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
